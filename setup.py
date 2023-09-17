44d2d7a85ddb3ffbd207061e669ebcc56227687d
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-09-17 04:02:45 +0300

    change data files in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='investws',
     version='0.0.1',
     packages=find_packages(),
-    data_files=glob.glob('investing/**'),
+    data_files=glob.glob('investws/**'),
     include_package_data=True,
     install_requires=[
         'websockets'
